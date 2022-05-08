7fb546ee06401c162683f5f620a96145d2c8834a
OmarSierraRico <sierrarico83@gmail.com>
2022-05-08 14:37:57 -0500

    Se sigue aprendiendo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders} from './app.routing';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { videojuegoComponent } from './videojuego/videojuego.component';
 import { zapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 
 @NgModule({
@@ -15,12 +17,14 @@ import { HomeComponent } from './home/home.component';
     videojuegoComponent,
     zapatillasComponent,
     CursosComponent,
-    HomeComponent
+    HomeComponent,
+    ContactoComponent
   
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [
     appRoutingProviders
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from "./home/home.component";
 import { zapatillasComponent } from "./zapatillas/zapatillas.component";
 import { videojuegoComponent } from "./videojuego/videojuego.component";
 import { CursosComponent } from "./cursos/cursos.component";
+import { ContactoComponent } from "./contacto/contacto.component";
+
 
 //Array de rutas 
 const appRoutes: Routes = [
@@ -14,6 +16,7 @@ const appRoutes: Routes = [
     { path: 'zapatillas', component: zapatillasComponent},
     { path: 'videojuego', component: videojuegoComponent},
     { path: 'cursos', component: CursosComponent},
+    { path: 'contacto', component: ContactoComponent },
     { path: '**', component: HomeComponent}
 ];
 
